c1e985d90dd6ae4d417ed16447c3cf3a9eab6536
Wisso1 <wisso1betty1@gmail.com>
2021-06-29 19:54:40 +0300

    test class

--- src/main/java/com/covalenthq/java_test/Main.java
+++ src/main/java/com/covalenthq/java_test/Main.java
@@ -8,7 +8,7 @@ import com.covalenthq.java.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		CovalentSession session=new CovalentSession("ckey_8b3b6571d83c49458ff461dfc32","json");
+		CovalentSession session=new CovalentSession("YOUR_API_KEY_HERE","json");
 		CovalentPricing p = new CovalentPricing(session);
 		//0x495f947276749Ce646f68AC8c248420045cb7b5e
 		p.getHistoricalByAddress(CovalentNetworks.BinanceSmartChain, CovalentQuoteCurrency.usd,"0xaA0C2e4fc2b57F60FceA0a5A7b09200C918fA71b");
