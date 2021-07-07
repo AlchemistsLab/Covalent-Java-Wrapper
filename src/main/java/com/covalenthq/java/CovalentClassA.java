25339299c2ad2bbe6f6d5a8643f0acc5ff032c3b
Gladys Mae Chiong <gladysmae.chiong@code-xpress.com>
2021-07-07 15:12:34 +0800

    remove comments.

--- src/main/java/com/covalenthq/java/CovalentClassA.java
+++ src/main/java/com/covalenthq/java/CovalentClassA.java
@@ -14,22 +14,6 @@ public class CovalentClassA {
 	// GET {chain_id}/address/{address}/balances_v2/
 	public String getTokenBalancesForAddress(CovalentNetworks cn,String address,boolean nft,boolean noNftFetch,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/address/"+address+"/balances_v2/";
-		/*if (nft) {
-			req+="?nft=true";
-		}
-		else {
-			req+="?nft=false";
-		}
-		if(noNftFetch) {
-			req+="&no-nft-fetch=true";
-		}
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"nft", "no-nft-fetch", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {nft? "true":"false", noNftFetch?"true": "false", match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -37,13 +21,6 @@ public class CovalentClassA {
 	// GET {chain_id}/address/{address}/portfolio_v2/
 	public String getHistoricalPortfolioValueOverTime(CovalentNetworks cn,String address,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/address/"+address+"/portfolio_v2/";
-		/*req+="?match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -51,25 +28,6 @@ public class CovalentClassA {
 	// GET {chain_id}/address/{address}/transactions_v2/
 	public String getTransactions(CovalentNetworks cn,String address,CovalentQuoteCurrency cqc,boolean block_signed_at_asc,boolean no_logs,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/address/"+address+"/transactions_v2/";
-		/*if (block_signed_at_asc) {
-			req+="?block-signed-at-asc=true";
-		}
-		else {
-			req+="?block-signed-at-asc=false";
-		}
-		if (no_logs) {
-			req+="&no-logs=true";
-		}
-		else {
-			req+="&no-logs=false";
-		}
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "block-signed-at-asc", "no-logs" ,"match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {block_signed_at_asc? "true": "false", no_logs? "true": false, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -77,14 +35,6 @@ public class CovalentClassA {
 	// GET {chain_id}/address/{address}/transfers_v2/
 	public String getERC20Transfers(CovalentNetworks cn,String address,String contract_address,CovalentQuoteCurrency cqc,boolean block_signed_at_asc,boolean no_logs,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/address/"+address+"/transfers_v2/";
-		/*req+="?contract-address="+contract_address;
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "contract-address", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {contract_address, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -92,13 +42,6 @@ public class CovalentClassA {
 	// GET {chain_id}/block_v2/{block_height}/
 	public String getBlock(CovalentNetworks cn,String blockHeight,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/block_v2/"+blockHeight+"/";
-		/*req+="?quote-currency="+cqc.getValue();
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -106,13 +49,6 @@ public class CovalentClassA {
 	// GET {chain_id}/block_v2/{start_date}/{end_date}/
 	public String getBlockHeights(CovalentNetworks cn,String start_date ,String end_date,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/block_v2/"+start_date+"/"+end_date+"/";
-		/*req+="?match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -120,15 +56,6 @@ public class CovalentClassA {
 	// GET {chain_id}/events/address/{address}/
 	public String getLogEventsByContractAddress(CovalentNetworks cn,String address,String starting_block,String ending_block,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/events/address/"+address+"/";
-		/*req+="?starting-block="+starting_block;
-		req+="&ending-block="+ending_block;
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"starting-block", "ending-block", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {starting_block, ending_block, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -136,17 +63,6 @@ public class CovalentClassA {
 	// GET {chain_id}/events/topics/{topic}/
 	public String getLogEventsByTopicHashes(CovalentNetworks cn,String topic,String secondary_topic,String starting_block,String ending_block,String sender_address,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/events/topics/"+topic+"/";
-		/*req+="?secondary-topic="+secondary_topic;
-		req+="&starting-block="+starting_block;
-		req+="&ending-block="+ending_block;
-		req+="&sender-address="+sender_address;
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"secondary-topic", "starting-block", "ending-block", "sender-address", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {secondary_topic, starting_block, ending_block, sender_address, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -154,13 +70,6 @@ public class CovalentClassA {
 	// GET {chain_id}/tokens/{address}/nft_metadata/{token_id}/
 	public String getExternalNFTMetadata(CovalentNetworks cn,String address,String token_id,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/tokens/"+address+"/nft_metadata/"+token_id+"/";
-		/*req+="?match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -168,13 +77,6 @@ public class CovalentClassA {
 	// GET {chain_id}/tokens/{address}/nft_token_ids/
 	public String getNFTTokensIDs(CovalentNetworks cn,String address,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/tokens/"+address+"/nft_token_ids/";
-		/*req+="?match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] { "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -182,14 +84,6 @@ public class CovalentClassA {
 	// GET {chain_id}/tokens/{address}/nft_transactions/{token_id}/
 	public String getNFTTransactions(CovalentNetworks cn,String address,String token_id,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/tokens/"+address+"/nft_transactions/"+token_id+"/";
-		/*req+="?match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;
-		return covSession.query(req);*/
 		String[] params = new String[] { "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -197,15 +91,6 @@ public class CovalentClassA {
 	// GET {chain_id}/tokens/{address}/token_holders_changes/
 	public String getChangesInTokenHolders(CovalentNetworks cn,String address,CovalentQuoteCurrency cqc,String starting_block,String ending_block,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/tokens/"+address+"/token_holders_changes/";
-		/*req+="?starting-block="+starting_block;
-		req+="&ending-block="+ending_block;
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"starting-block", "ending-block", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {starting_block, ending_block, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -214,14 +99,6 @@ public class CovalentClassA {
 	// GET {chain_id}/tokens/{address}/token_holders/
 	public String getTokenHoldersASOfBlockHeight(CovalentNetworks cn,String address,String block_height,CovalentQuoteCurrency cqc,String starting_block,String ending_block,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/tokens/"+address+"/token_holders/";
-		/*req+="?block-height="+starting_block;
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"starting-block", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {starting_block, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -229,14 +106,6 @@ public class CovalentClassA {
 	// GET {chain_id}/tokens/tokenlists/{id}/
 	public String getAllContractMetaData(CovalentNetworks cn,String id,String block_height,CovalentQuoteCurrency cqc,String starting_block,String ending_block,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/tokens/tokenlists/"+id+"/";
-		/*req+="?block-height="+starting_block;
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"starting-block", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {starting_block, match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
@@ -244,20 +113,6 @@ public class CovalentClassA {
 	// GET {chain_id}/transaction_v2/{tx_hash}/
 	public String getTransaction(CovalentNetworks cn,String tx_hash,boolean no_logs,CovalentQuoteCurrency cqc,String primer,String match,String group,String sort,int skip,int limit) throws IOException {
 		String req = cn.getValue()+"/transaction_v2/"+tx_hash+"/";
-		/*if(no_logs) {
-			req+="?no-logs=true";
-		}
-		else  {
-			req+="?no-logs=false";
-		}
-		
-		req+="&match="+match;
-		req+="&group="+group;
-		req+="&sort="+sort;
-		req+="&skip="+skip;
-		req+="&primer="+primer;
-		req+="&quote-currency="+cqc.getValue();
-		req+="&limit="+limit;*/
 		String[] params = new String[] {"no-logs", "match", "group", "sort", "skip", "primer", "quote-currency", "limit"};
 		Object[] paramValues = new Object[] {no_logs? "true": "false", match, group, sort, skip, primer, cqc.getValue(), limit };
 		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
