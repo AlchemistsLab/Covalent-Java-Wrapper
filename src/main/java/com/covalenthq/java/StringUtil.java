292dc54ad7f7a13dbe49dcc32607216ba1628a45
Gladys Mae Chiong <gladysmae.chiong@code-xpress.com>
2021-07-07 21:54:45 +0800

    remove useless space

--- src/main/java/com/covalenthq/java/StringUtil.java
+++ src/main/java/com/covalenthq/java/StringUtil.java
@@ -4,15 +4,11 @@ public  class StringUtil {
 
 	//a more dynamic way to concatenate url parameters 
 	public static String ConcatUrlParams(String url, String[] params, Object[] paramValues) {
-		
 		if(params.length != paramValues.length) return url;
-		
-		
 		for(int i=0; i< params.length; i++) {
 			String separator= i==0? "?": "&";
 			url+= separator+params[i]+"="+paramValues[i];
 		}
-		
 		return url;
 	}
 	
