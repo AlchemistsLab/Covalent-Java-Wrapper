d39cfdc23ab4053788afa43bdda6fea8f034d03b
Gladys Mae Chiong <gladysmae.chiong@code-xpress.com>
2021-07-07 15:09:18 +0800

    dynamic concatenation url parameters on classA

---
+++ src/main/java/com/covalenthq/java/StringUtil.java
@@ -0,0 +1,20 @@
+package com.covalenthq.java;
+
+public  class StringUtil {
+
+	//a more dynamic way to concatenate url parameters 
+	public static String ConcatUrlParams(String url, String[] params, Object[] paramValues) {
+		
+		if(params.length != paramValues.length) return url;
+		
+		
+		for(int i=0; i< params.length; i++) {
+			String separator= i==0? "?": "&";
+			url+= separator+params[i]+"="+paramValues[i];
+		}
+		
+		return url;
+	}
+	
+	 
+}
