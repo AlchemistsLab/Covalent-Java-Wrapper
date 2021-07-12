804ee6b810d28746ec7dea5474d5b06c6a4fb638
Gladys <86119120+kukorokie@users.noreply.github.com>
2021-07-12 15:25:57 +0800

    concat url in class b (#2)
    concat string url for class b

--- src/main/java/com/covalenthq/java/CovalentClassB.java
+++ src/main/java/com/covalenthq/java/CovalentClassB.java
@@ -19,15 +19,16 @@ public class CovalentClassB {
 	// GET {chain_id}/address/{address}/stacks/sushiswap/acts/
 	public String getSushiswapAddressExchangeLiquidityTransactions (CovalentNetworks cn,String address,CovalentQuoteCurrency cqc,String swaps) throws IOException {
 		String req = cn.getValue()+"/address/"+address+"/stacks/sushiswap/acts/";
-		req+="?quote-currency="+cqc.getValue();
-		req+="&swaps="+swaps;
-		return covSession.query(req);
+		String[] params = new String[] { "quote-currency", "swaps"};
+		Object[] paramValues = new Object[] {cqc.getValue(), swaps };
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET {chain_id}/address/{address}/stacks/sushiswap/balances/
 	public String getSushiswapAddressExchange (CovalentNetworks cn,String address,CovalentQuoteCurrency cqc) throws IOException {
 		String req = cn.getValue()+"/address/"+address+"/stacks/sushiswap/balances/";
-		req+="?quote-currency="+cqc.getValue();
-		return covSession.query(req);
+		String[] params = new String[] { "quote-currency"};
+		Object[] paramValues = new Object[] {cqc.getValue()};
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET {chain_id}/networks/aave_v2/assets/
 	public String getAaveV2NetworkAssets (CovalentNetworks cn) throws IOException {
@@ -37,8 +38,9 @@ public class CovalentClassB {
 	// GET {chain_id}/networks/sushiswap/assets/
 	public String getSushiswapNetworkAssets (CovalentNetworks cn,String tickers) throws IOException {
 		String req = cn.getValue()+"/networks/aave_v2/assets/";
-		req+="?tickers="+tickers;
-		return covSession.query(req);
+		String[] params = new String[] { "tickers"};
+		Object[] paramValues = new Object[] {tickers };
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET 1/address/{address}/stacks/aave_v2/balances/
 	public String getAaveV2AddressBalance (CovalentNetworks cn,String address) throws IOException {
@@ -83,8 +85,9 @@ public class CovalentClassB {
 	// GET 1/address/{address}/stacks/uniswap_v2/acts/
 	public String getUniswapV2AddressLquidityTransactions (String address,String swaps) throws IOException {
 		String req = "1/address/"+address+"/stacks/uniswap_v2/acts/";
-		req+="?swaps="+swaps;
-		return covSession.query(req);
+		String[] params = new String[] {  "swaps"};
+		Object[] paramValues = new Object[] { swaps };
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET 1/address/{address}/stacks/uniswap_v2/balances/
 	public String getUniswapV2AddressExchangeBalances (String address) throws IOException {
@@ -114,28 +117,30 @@ public class CovalentClassB {
 	// GET 56/address/{address}/stacks/pancakeswap_v2/balances/
 	public String getPancakeswapV2AddressExchangeBalances (String address,CovalentQuoteCurrency cqc) throws IOException {
 		String req = "56/address/"+address+"/stacks/pancakeswap_v2/balances/";
-		req+="?quote-currency="+cqc.getValue();
-		return covSession.query(req);
+		String[] params = new String[] { "quote-currency"};
+		Object[] paramValues = new Object[] {cqc.getValue()};
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET 56/address/{address}/stacks/pancakeswap/acts/
 	public String getPancakeswapV2AddressExchangeLiquidityTransactions (String address,CovalentQuoteCurrency cqc,String swaps) throws IOException {
 		String req = "56/address/"+address+"/stacks/pancakeswap/acts/";
-		req+="?quote-currency="+cqc.getValue();
-		req+="&swaps="+swaps;
-		return covSession.query(req);
+		String[] params = new String[] { "quote-currency", "swaps"};
+		Object[] paramValues = new Object[] {cqc.getValue(), swaps };
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET 56/address/{address}/stacks/pancakeswap/balances/
 	public String getPancakeswapAddressExchangeBalances (String address,CovalentQuoteCurrency cqc) throws IOException {
 		String req = "56/address/"+address+"/stacks/pancakeswap/balances/";
-		req+="?quote-currency="+cqc.getValue();
-		return covSession.query(req);
+		String[] params = new String[] { "quote-currency"};
+		Object[] paramValues = new Object[] {cqc.getValue()};
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET 56/networks/pancakeswap_v2/assets/
 	public String getPancakeswapV2NetworkAssets (String tickers,String contract_addresses) throws IOException {
 		String req = "56/networks/pancakeswap_v2/assets/";
-		req+="?tickers="+tickers;
-		req+="&contract-addresses="+contract_addresses;
-		return covSession.query(req);
+		String[] params = new String[] { "tickers", "contract-addresses"};
+		Object[] paramValues = new Object[] {tickers, contract_addresses };
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 	// GET 56/networks/pancakeswap_v2/assets/{address}/
 	public String getPancakeswapV2NetworkAssetsByAddress (String addresses) throws IOException {
@@ -145,7 +150,8 @@ public class CovalentClassB {
 	// GET 56/networks/pancakeswap/assets/
 	public String getPancakeswapNetworkAssets (String tickers) throws IOException {
 		String req = "56/networks/pancakeswap/assets/";
-		req+="?tickers="+tickers;
-		return covSession.query(req);
+		String[] params = new String[] { "tickers"};
+		Object[] paramValues = new Object[] {tickers};
+		return covSession.query(StringUtil.ConcatUrlParams(req, params, paramValues));
 	}
 }
